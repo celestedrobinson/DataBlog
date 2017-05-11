2503df52b7ea2df7d4e6461c98d187463cbf29da
Bradley Robinson <bradley.s.robinson12@gmail.com>
2017-05-11 12:58:30 -0600

    Changed formatting to be more beautiful

--- hello.py
+++ hello.py
@@ -9,6 +9,25 @@ import urllib.request
 
 app = Flask(__name__)
 
+
+blog_posts = [{'title': "Finding Their Party: Using Clustering to Find Patterns in Data",
+               'address':
+               "http://robinsonbrain.blogspot.com/2017/04/finding-their-party-using-voting-to.html",
+               'date': '4/27/2017',
+               'description': 'Using voting data from Utah\'s House of Representatives, I describe'\
+               'how the K-Means algorithm work. Complete with cool, interactive visualizations of'\
+               'the algorithm during each step, as well as a cute dog picture. Links to code are'\
+               'provided.'},
+              {'title': "Who's Utah's 'Yes Man?': Data Insights into Utah's House of "\
+               "Representatives 2017",
+               'address':
+               "http://robinsonbrain.blogspot.com/2017/02/grouping-members-of-utahs-house-of.html",
+               'date': '2/19/2017',
+               'description': 'Ever curious about who votes like your representative in Utah\'s '\
+               'House of Representatives? I collect and analyze voting data to find out '\
+               'interesting patterns in the data.'}
+             ]
+
 @app.route("/about")
 def about():
     """
@@ -28,10 +47,11 @@ def contact():
 @app.route("/posts")
 def posts():
     """
-    This will have all my posts. Right now I haven't added any. Expect to see some interesting
-    content soon.
+    This will have all my posts. This should be dynamic so as I increase the amount
+    of posts, they will be on multiple pages.
     """
-    return render_template("posts.html")
+    return render_template("posts.html",
+                           blog_posts=blog_posts)
 
 @app.route("/")
 def home():
