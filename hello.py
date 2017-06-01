acb374c8906aaf78d7f05851daf35f34c8144725
Bradley Robinson <bradley.s.robinson12@gmail.com>
2017-06-01 07:43:19 -0600

    Added error pages

--- hello.py
+++ hello.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from flask import render_template
-from flask import request
+from flask import render_template, request, redirect, url_for
 import json
 import os
 import platform
@@ -17,9 +16,9 @@ blog_posts = [{'title': "Finding Their Party: Using Clustering to Find Patterns 
                "http://robinsonbrain.blogspot.com/2017/04/finding-their-party-using-voting-to.html",
                'date': '4/27/2017',
                'description': 'Using voting data from Utah\'s House of Representatives, I describe'\
-               'how the K-Means algorithm work. Complete with cool, interactive visualizations of'\
-               'the algorithm during each step, as well as a cute dog picture. Links to code are'\
-               'provided.'},
+               ' how the K-Means algorithm work. Complete with cool, interactive visualizations of'\
+               ' the algorithm during each step, as well as a cute dog picture. Links to code are'\
+               ' provided.'},
               {'title': "Who's Utah's 'Yes Man?': Data Insights into Utah's House of "\
                "Representatives 2017",
                'address':
@@ -29,6 +28,14 @@ blog_posts = [{'title': "Finding Their Party: Using Clustering to Find Patterns 
                'House of Representatives? I collect and analyze voting data to find out '\
                'interesting patterns in the data.'}
              ]
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect(url_for('error_page'))
+
+@app.route("/error")
+def error_page():
+    return render_template('page_not_found.html'), 404
 
 @app.route("/about")
 def about():
@@ -66,7 +73,10 @@ def blog_post(post_id):
                                            ))
     else:
         path = os.path.abspath(os.path.join("static", "posts", "{}.html".format(post_id)))
-    html = open(path, encoding="utf-8").read()
+    try:
+        html = open(path, encoding="utf-8").read()
+    except FileNotFoundError:
+        return redirect(url_for('error_page'))
     return render_template("default_post.html",
                            content=html)
 
