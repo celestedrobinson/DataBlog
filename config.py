1a63b8eda8befb08987f57401612279e83cf3d78
Bradley Robinson <bradley.s.robinson12@gmail.com>
2017-06-17 20:08:49 -0600

    Added config file

---
+++ config.py
@@ -0,0 +1,26 @@
+"""
+Data for app
+"""
+
+blog_posts = [{'title': "Finding Their Party: Using Clustering to Find Patterns in Data",
+               'address':
+               "http://robinsonbrain.blogspot.com/2017/04/finding-their-party-using-voting-to.html",
+               'date': '4/27/2017',
+               'description': 'Using voting data from Utah\'s House of Representatives, I describe'\
+               ' how the K-Means algorithm work. Complete with cool, interactive visualizations of'\
+               ' the algorithm during each step, as well as a cute dog picture. Links to code are'\
+               ' provided.'},
+              {'title': "Who's Utah's 'Yes Man?': Data Insights into Utah's House of "\
+               "Representatives 2017",
+               'address':
+               "http://robinsonbrain.blogspot.com/2017/02/grouping-members-of-utahs-house-of.html",
+               'date': '2/19/2017',
+               'description': 'Ever curious about who votes like your representative in Utah\'s '\
+               'House of Representatives? I collect and analyze voting data to find out '\
+               'interesting patterns in the data.'}
+             ]
+
+datasets = {'game': 'Video Games',
+            'svotes': 'U.S. Senate Votes',
+            'mcd': 'McDonalds Nutritional Facts'}
+
